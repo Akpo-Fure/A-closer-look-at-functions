5ec06f6440c081d049a687aa75ec3c82ea777330
Akpo_Fure <okegbeakpofurekelvin@gmail.com>
2023-07-09 14:17:05 +0100

    How Passing Argument Works

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 'use strict';
 
-//Default Parameters
-const bookings = []
-const createBooking = function(flightNum, numPassangers = 1, price = 199 * numPassangers){
-    // ES5
-    // numPassangers = numPassangers || 1
-    // price = price || 199
+// //Default Parameters
 
-    const booking = {
-        flightNum,
-        numPassangers,
-        price
-    }
-    console.log(booking)
-    bookings.push(booking)
+// const bookings = []
+// const createBooking = function(flightNum, numPassangers = 1, price = 199 * numPassangers){
+//     // ES5
+//     // numPassangers = numPassangers || 1
+//     // price = price || 199
+
+//     const booking = {
+//         flightNum,
+//         numPassangers,
+//         price
+//     }
+//     console.log(booking)
+//     bookings.push(booking)
+// }
+// createBooking("LH123")
+// createBooking("LH123", 2, 800)
+// createBooking("LH123", 2)
+// createBooking("LH123", 5)
+// createBooking("LH123", undefined ,1000)
+
+
+//How Passing Arguments Work
+
+const flight = "LH234"
+const Akpofure = {
+    name: "Okegbe Akpofure",
+    passport: 2348147567515
+
 }
+const checkIn = function(flightNum, passenger){
+  flightNum = "LH999"
+  passenger.name = "Mr. " + passenger.name
 
-createBooking("LH123")
-createBooking("LH123", 2, 800)
-createBooking("LH123", 2)
-createBooking("LH123", 5)
-createBooking("LH123", undefined ,1000)
+  if(passenger.passport === 2348147567515){
+    alert("checked in")
+  }else{
+    alert("Wrong passport")
+  }
+}
+checkIn(flight, Akpofure)
+console.log(flight)
+console.log(Akpofure)
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random() + 1000000000)
+}
+newPassport(Akpofure)
+checkIn(flight, Akpofure)
